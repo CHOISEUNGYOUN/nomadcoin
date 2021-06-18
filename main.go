be93bca5f54361523fbdfc08b6aa93e77c48d1ae
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-06-18 23:58:14 +0900

    working on building templates

--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/choiseungyoun/nomadcoin/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello from home!")
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
