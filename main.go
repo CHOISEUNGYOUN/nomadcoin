72d64e0dca832fabc4e3ea8c9aff37eebf947711
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-07-06 23:52:15 +0900

    add cli

--- main.go
+++ main.go
@@ -1,42 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welcome to Nomad Coin!\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer: Start the HTML Explorer\n")
-	fmt.Printf("rest:     Start the REST API (recommended)\n\n")
-	os.Exit(0)
-}
+import "github.com/choiseungyoun/nomadcoin/cli"
 
 func main() {
-
-	fmt.Println(os.Args[2:])
-
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of the server (default 4000)")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Println(*portFlag)
-		fmt.Println("Start server")
-	}
+	cli.Start()
 }
