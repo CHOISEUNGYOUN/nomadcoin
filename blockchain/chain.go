50ce627baf4719793c1b2cd537cfe0f89abc4e70
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-07-25 20:53:32 +0900

    add mining func

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -7,9 +7,17 @@ import (
 	"github.com/choiseungyoun/nomadcoin/utils"
 )
 
+const (
+	defaultDifficulty  int = 2
+	difficultyInterval int = 5
+	blockInterval      int = 2
+	allowedRange       int = 2
+)
+
 type blockchain struct {
-	NewestHash string `json:"newest_hash"`
-	Height     int    `json:"height"`
+	NewestHash        string `json:"newest_hash"`
+	Height            int    `json:"height"`
+	CurrentDifficulty int    `json:"current_difficulty"`
 }
 
 var b *blockchain
@@ -20,13 +28,15 @@ func (b *blockchain) restore(data []byte) {
 }
 
 func (b *blockchain) persist() {
-	db.SaveBlockchain(utils.ToBytes(b))
+	db.SaveCheckpoint(utils.ToBytes(b))
 }
 
 func (b *blockchain) AddBlock(data string) {
-	block := createBlock(data, b.NewestHash, b.Height+1)
+	block := createBlock(data, b.NewestHash, b.Height+1, 2)
 	b.NewestHash = block.Hash
 	b.Height = block.Height
+	b.CurrentDifficulty = block.Difficulty
+	b.persist()
 }
 
 func (b *blockchain) Blocks() []*Block {
@@ -44,10 +54,36 @@ func (b *blockchain) Blocks() []*Block {
 	return blocks
 }
 
+func (b *blockchain) recalculateDifficulty() int {
+	allBlocks := b.Blocks()
+	newestBlock := allBlocks[0]
+	lastRecalculatedBlock := allBlocks[difficultyInterval-1]
+	actualTime := (newestBlock.Timestamp / 60) - (lastRecalculatedBlock.Timestamp / 60)
+	expectedTime := difficultyInterval * blockInterval
+
+	if actualTime < (expectedTime - allowedRange) {
+		return b.CurrentDifficulty + 1
+	} else if actualTime > (expectedTime + allowedRange) {
+		return b.CurrentDifficulty - 1
+	}
+	return b.CurrentDifficulty
+}
+
+func (b *blockchain) difficulty() int {
+	if b.Height == 0 {
+		return defaultDifficulty
+	} else if b.Height%difficultyInterval == 0 {
+		return b.recalculateDifficulty()
+	}
+	return b.CurrentDifficulty
+}
+
 func Blockchain() *blockchain {
 	if b == nil {
 		once.Do(func() {
-			b = &blockchain{"", 0}
+			b = &blockchain{
+				Height: 0,
+			}
 			checkpoint := db.Checkpoint()
 			if checkpoint == nil {
 				b.AddBlock("Genesis")
