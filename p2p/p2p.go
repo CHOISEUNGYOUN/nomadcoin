6043e0e981f8cdc5eae835b151557d89d1dafad4
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-08-13 23:41:11 +0900

    wip of websocket-2

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,7 +1,9 @@
 package p2p
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/choiseungyoun/nomadcoin/utils"
 	"github.com/gorilla/websocket"
@@ -10,6 +12,20 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	_, err := upgrader.Upgrade(rw, r, nil)
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			conn.Close()
+			break
+		}
+		fmt.Printf("just got:%s\n\n", p)
+		time.Sleep(5 * time.Second)
+		message := fmt.Sprintf("New message: %s", p)
+		utils.HandleErr(conn.WriteMessage(websocket.TextMessage, []byte(message)))
+	}
 }
