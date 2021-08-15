a48ddfe82cdd0456a67a855d632130c85ca9a5cf
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-08-15 17:22:49 +0900

    done with socket testing

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/choiseungyoun/nomadcoin/utils"
 	"github.com/gorilla/websocket"
@@ -12,20 +11,18 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
+	open_port := r.URL.Query().Get("open_port")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		return open_port != "" || ip != ""
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	for {
-		_, p, err := conn.ReadMessage()
-		if err != nil {
-			conn.Close()
-			break
-		}
-		fmt.Printf("just got:%s\n\n", p)
-		time.Sleep(5 * time.Second)
-		message := fmt.Sprintf("New message: %s", p)
-		utils.HandleErr(conn.WriteMessage(websocket.TextMessage, []byte(message)))
-	}
+	initPeer(conn, ip, open_port)
+}
+
+func AddPeer(address, port, open_port string) {
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?open_port=%s", address, port, open_port[1:]), nil)
+	utils.HandleErr(err)
+	initPeer(conn, address, port)
 }
